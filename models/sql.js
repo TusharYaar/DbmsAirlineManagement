ce03bfaca20266385ffa7824a33f0ceab864d5d2
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2020-09-29 11:52:25 +0530

    Connected to Online Database

--- models/sql.js
+++ models/sql.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'tushar',
-    database: 'flightdatabase'
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'be2aea29eef7b6',
+    password: '85e2a4a6',
+    database: 'heroku_a30f9d24d78dbdd'
 });
 
 
