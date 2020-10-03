d6eb0ded37977229312602888b5aaa9a0a395428
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2020-10-03 23:23:28 +0530

    add route from searchflight

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -14,7 +14,7 @@ var checkUser = (req, res, next) => {
     if (req.session.userid && req.session.usertype == "user") {
         next();
     } else {
-        res.redirect("Dashboard");
+        res.redirect("/dashboard");
     }
 };
 router.get("/searchflight", function(req, res) {
@@ -45,6 +45,23 @@ router.post("/searchflight", function(req, res) {
     });
 });
 
+router.get("/searchflight/:flight",checkUser,function(req, res){
+    flight = req.params.flight;
+    sql = " select flight_number,departure_date,departure_time,arrival_date,arrival_time, " +
+    "ap_des.airport_name as des_name, ap_des.airport_state as des_state, ap_des.airport_city as des_city, ap_des.airport_short as des_short, " +
+    "ap_dep.airport_name as dep_name, ap_dep.airport_state as dep_state, ap_dep.airport_city as dep_city, ap_dep.airport_short as dep_short " +
+    "from flight join airport as ap_dep on flight.departure = ap_dep.airport_id join airport as ap_des on flight.destination = ap_des.airport_id " +
+    "WHERE flight.flight_number = ?"
+    connection.query(sql,[flight],function(err, result, fields){
+        if(err) {
+            console.log(err);
+            res.send("Error");
+        }
+        else {
+            res.send(result);
+        }
+    });
+});
 
 
 module.exports = router;
