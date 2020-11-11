ac624057f53fa7ac37aac2217f640e4a73d211d7
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2020-11-11 16:36:34 +0530

    Add Readme
    Add readme
    Remove datails for database

---
+++ README.md
@@ -0,0 +1,9 @@
+This Is A Project For My DBMS (DataBase Management Systems) Subject
+It uses HTML, CSS, JavaScript, Nodejs and SQL
+
+1. Clone the repository
+2. Goto Models ->sql.js and fill the details for your database
+3. Run Command in the terminal -> "npm init"
+   (note you must have npm and node installed)
+4. Run Command in the terminal -> "npm start"
+5. Goto localhost:3000
--- models/sql.js
+++ models/sql.js
@@ -1,35 +1,33 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 var db_config = {
-    host: 'sm9j2j5q6c8bpgyq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'ow9sfihrl1htyxak',
-    password: 'i2zdwwk5ypiwepjv',
-    database: 'vgloi02hni56ec3w'
+  host: "", // Host name   Eg. localhost
+  user: "", // Username in the database    Eg. root
+  password: "", // Password in the database Eg. password
+  database: "", // Database You want to connect to   Eg. Flight Database
 };
-
-var db_config_local = {
-    host: 'localhost',
-    user: 'root',
-    password: 'tushar',
-    database: 'flightdatabase'
-};
+// If you want to connect to an Online database Fill the details accodingly
 var connection;
 
 function handleDisconnect() {
-    connection = mysql.createConnection(db_config);
-    connection.connect(function(err) { // The server is either down
-        if (err) { // or restarting (takes a while sometimes).
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 4000); // We introduce a delay before attempting to reconnect,
-        } else { console.log('connection Successfull'); } // to avoid a hot loop, and to allow our node script to
-    }); // process asynchronous requests in the meantime.
-    connection.on('error', function(err) {
-        console.log('db error', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect();
-        } else {
-            throw err;
-        }
-    });
+  connection = mysql.createConnection(db_config);
+  connection.connect(function (err) {
+    // The server is either down
+    if (err) {
+      // or restarting (takes a while sometimes).
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 4000); // We introduce a delay before attempting to reconnect,
+    } else {
+      console.log("connection Successfull");
+    } // to avoid a hot loop, and to allow our node script to
+  }); // process asynchronous requests in the meantime.
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
 }
 
 handleDisconnect();
